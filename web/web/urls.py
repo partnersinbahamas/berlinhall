a4fdbb9321099da8183a844e95a7e144d253d4de
Denys Bokov <denisbokov1703@gmail.com>
2025-01-27 17:56:49 +0100

    feat: load setting media and static files to urls

--- web/web/urls.py
+++ web/web/urls.py
@@ -18,12 +18,17 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('user/', include('user.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
