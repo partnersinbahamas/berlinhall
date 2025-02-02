dd7c837fd14cf729b12582f3374b5b0b6e6447a1
Denys Bokov <denisbokov1703@gmail.com>
2025-02-02 13:45:35 +0100

    feat: add paginations for posts at home page

--- web/blog/views.py
+++ web/blog/views.py
@@ -8,6 +8,7 @@ class Home(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
+    paginate_by = 4
 
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
